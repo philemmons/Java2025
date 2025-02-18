7ac41790c9387d2f7e213a98d15dd2123ad53976
philemmons <phil_emmons@sbcglobal.net>
2025-02-18 15:21:13 -0800

    added jdbc connector rar

---
+++ database_example/src/com/philemmons/JavaTutDatabase.java
@@ -0,0 +1,10 @@
+package com.philemmons;
+
+import java.sql.Connection;
+
+public class JavaTutDatabase {
+    public static void main(String[] args) throws Exception {
+        Connect con;
+
+    }
+}
